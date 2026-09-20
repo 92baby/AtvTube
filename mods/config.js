c33de5553a475dc1fecc3e72345b5a5f9d1199f6
Joshuagpt <ca0g2m0@gmail.com>
2026-09-20 11:00:34 +0800

    Add files via upload

--- mods/config.js
+++ mods/config.js
@@ -20,6 +20,7 @@ const defaultConfig = {
   focusContainerColor: '#0f0f0f',
   routeColor: '#0f0f0f',
   enableFixedUI: (window.h5vcc) ? false : true,
+  lastResolvedUILanguage: null,
   enableHqThumbnails: false,
   enableChapters: true,
   enableLongPress: true,
